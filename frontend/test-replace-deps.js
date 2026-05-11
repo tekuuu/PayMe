0d8d655f2c9c9aa55b7157fc5a6300d98f64a4a1
tekuuu <tekalign3330@gmail.com>
2026-05-11 22:56:28 +0300

    chore: remove scratch test files and build artifacts

--- frontend/test-replace-deps.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const path = 'src/hooks/use-confidential-balance.ts';
-let code = fs.readFileSync(path, 'utf8');
-code = code.replace(
-    /}, \[cardCusdcAddress, balanceHandle\]\);/,
-    "}, [cardAddress, cardCusdcAddress, balanceHandle]);"
-);
-fs.writeFileSync(path, code);
